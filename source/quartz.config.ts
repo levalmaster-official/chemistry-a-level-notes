a195245d22a57c73afbaec1b307e80ad3752414a
levalmaster <hey.gbp@gmail.com>
2024-11-30 21:51:14 +0000

    Update config file to change header

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "OCR A Chemistry ⌬",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "en-UK",
+    baseUrl: "levalmaster-official.github.io/chemistry-a-level-notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
